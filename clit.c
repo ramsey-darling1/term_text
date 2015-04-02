03b55a3da31ccd8d5171d833a296c86ce9162c35
Ramsey <rdarling42@gmail.com>
2015-04-02 15:13:36 -0400

    breaking out things into functions

--- clit.c
+++ clit.c
@@ -6,14 +6,30 @@
 
 #include<stdio.h>
 
-main(){
-    
-    //first grab number for who we want to text
+int get_num(){
+    //grab the phone number we want to text
     int phone_number;
     printf("Please enter the number you wish to text: ");
     scanf("%d", &phone_number);
-    printf("Now sending a text message to: %d", phone_number);
     getchar();
+    return phone_number;
+}
 
+int res_message(char *message, int phone_number) {
+    //give the user a response message
+    printf("%s: %i\n", message, phone_number);
     return 0;
 }
+
+int main(){
+
+    int phone_number;
+    
+    phone_number = get_num();
+
+    res_message("Here is the number you want to text",phone_number);
+    
+    return 0;
+}
+
+
