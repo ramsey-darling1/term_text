4d3fb6c347752bc195979219df3db3ba7699ee18
Ramsey <rdarling42@gmail.com>
2015-04-30 14:05:42 -0400

    almost working, removing first character from string for some reason

--- clit.c
+++ clit.c
@@ -7,29 +7,27 @@
 #include <stdio.h>
 #include <curl/curl.h>
 
-char * get_num(){
+struct text_message {
+    char phone_number[10];
+    char message[140];
+
+};
+
+char * get_num(char * phone_number){
     //grab the phone number we want to text
     //number has to be represented as a string
-    char phone_number[10];
     printf("Please enter the 10 digit number you wish to text, no spaces or special characters: ");
-    return fgets(phone_number,10,stdin);
+    getchar();
+    fgets(phone_number,10,stdin);
+    return phone_number;
 }
 
-char * get_message(){
+char * get_text_message(char * text_message){
     //grab the message that the user wants to send
-    char mess[140];
     printf("Please enter the message you wish to send, no more than 140 characters: ");
-    return fgets(mess,140,stdin);
-}
-
-int varify_phone_number(int phone_number){
-    int res;
-    if(phone_number < 1000000000 || phone_number > 9999999999){
-        res = 0;//it's not really a phone number 
-    }else{
-        res = 1; 
-    }
-    return res;
+    getchar();
+    fgets(text_message,140,stdin);
+    return text_message;
 }
 
 int res_message(char * message, char * phone_number) {
@@ -38,7 +36,7 @@ int res_message(char * message, char * phone_number) {
     return 0;
 }
 
-int request(int phone_number, char *text_message){
+int request(char * phone_number, char * text_message){
     CURL *curl;
     CURLcode res;
     int result;
@@ -47,10 +45,10 @@ int request(int phone_number, char *text_message){
 
     if(curl){
         curl_easy_setopt(curl, CURLOPT_URL,"http://textbelt.com/text");
-        //curl_easy_setopt(curl,CURLOPT_POSTFIELDS,("number=%i",phone_number));
-        //curl_easy_setopt(curl,CURLOPT_POSTFIELDS,("message=%s",text_message));
-        curl_easy_setopt(curl,CURLOPT_POSTFIELDS,"number=7342395992");
-        curl_easy_setopt(curl,CURLOPT_POSTFIELDS,"message=this is just a test");
+        curl_easy_setopt(curl,CURLOPT_POSTFIELDS,("number=%s",phone_number));
+        curl_easy_setopt(curl,CURLOPT_POSTFIELDS,("message=%s",text_message));
+        //curl_easy_setopt(curl,CURLOPT_POSTFIELDS,"number=7342395992");
+        //curl_easy_setopt(curl,CURLOPT_POSTFIELDS,"message=this is just a test");
         curl_easy_setopt(curl,CURLOPT_FOLLOWLOCATION,1l);//follow redirection if redirected
         //preform the actual request
         res = curl_easy_perform(curl);
@@ -65,27 +63,18 @@ int request(int phone_number, char *text_message){
 
 int main(){
 
-    char * phone_number;
-    char * message;
-    char * text_message;
+    struct text_message active;
+    //char * response_message;
 
-    phone_number = get_num();
-    text_message = get_message();
+    get_num(active.phone_number);
+    get_text_message(active.message);
 
-    /*switch(varify_phone_number(phone_number)){
-        case 1:
-            request(phone_number,text_message);
-            message = "Thanks, message was sent to: ";
-            break;
-        case 0:
-            message = "Sorry, that is not a valid phone number, we can not send a text to that number.";
-            break;
-    }*/
+    //request(active.phone_number,active.message);
 
-    message = "just a test";
+    //response_message = "just a test";
 
-    printf("called from main: %s\n",phone_number);
-    res_message(message,phone_number);
+    printf("called from main: %s, %s\n",active.phone_number,active.message);
+    //res_message(message,phone_number);
 
     return 0;
 }
